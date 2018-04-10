e0bac0c6ed207e5c9c9220485b037675a7526424
Antos23 <35873201+Antos23@users.noreply.github.com>
2018-04-10 18:48:20 +0100

    profile card js effects
    TO FIX:
    only one function is needed

--- main.js
+++ main.js
@@ -6,3 +6,42 @@ $("#menu-toggle").click(function(e) {
 $('.carousel').carousel({
   interval: 5000,
 });
+
+/*profile cards*/
+
+
+$(document).ready(function(){
+    $("#belena").click(function(){
+        $("#elena").slideToggle("slow");
+
+    });
+});
+$(document).ready(function(){
+    $("#bluca").click(function(){
+        $("#luca").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $("#bjosh").click(function(){
+        $("#josh").slideToggle("slow");
+
+    });
+});
+
+
+$(document).ready(function(){
+    $("#bsilvia").click(function(){
+        $("#silvia").slideToggle("slow");
+
+    });
+});
+
+$(document).ready(function(){
+    $("#blaura").click(function(){
+        $("#laura").slideToggle("slow");
+
+    });
+});
+
